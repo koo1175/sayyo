6794249e8df8907718bb00335fb02c23d8eede67
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-11-05 22:41:25 +0900

    Create App.js

---
+++ sayo/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from "./views/Navigation";
+import Main from "./views/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Navigation/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
