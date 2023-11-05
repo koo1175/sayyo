d23d9822fc2a220a8b4bdb765a7a0a154c647f99
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-11-05 22:42:04 +0900

    Create index.js

---
+++ sayo/src/views/Main/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main() {
+
+    return (
+        <div style={{width: '100%',
+            height: '100%',
+            backgroundColor:'blue',
+            backgroundSize: 'cover',
+            color: 'white',
+            fontSize: '70px',
+            marginTop:'20px'}}>
+            MainPage
+        </div>
+        );
+}
