5d1351fefc621ebdd7298755e518cf92525c0d26
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-11-05 22:42:00 +0900

    Create index.js

---
+++ sayo/src/views/Community/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Community() {
+
+    return (
+
+        <div style={{width: '100%',
+            height: '100%',
+            backgroundColor:'black',
+            backgroundSize: 'cover',
+            color: 'white',
+            fontSize: '70px',
+            marginTop:'20px'}}>
+            CommunityPage
+        </div>
+
+        );
+}
