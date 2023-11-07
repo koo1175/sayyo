86514ae502ad9566b50ecaf793521aa5be90789e
youngsil <koo1175@naver.com>
2023-11-07 17:10:11 +0900

    second-commit: 좋아요 작업 ( 시장님, 댓글 )

---
+++ src/main/java/com/pj/sayyo/model/rLike/mapper/RLikeMapper.java
@@ -0,0 +1,21 @@
+package com.pj.sayyo.model.rLike.mapper;
+
+import com.pj.sayyo.model.rLike.dto.RLikeDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface RLikeMapper {
+    int like(RLikeDto rLikeDto);
+    int reLike(RLikeDto rLikeDto);
+    int dislike(RLikeDto rLikeDto);
+    int reDislike(RLikeDto rLikeDto);
+    int cancelLike(RLikeDto rLikeDto);
+    int reCancelLike(RLikeDto rLikeDto);
+    int reCancelDislike(RLikeDto rLikeDto);
+    int findLike(RLikeDto rLikeDto);
+    int deleteLike(RLikeDto rLikeDto);
+    RLikeDto findState(RLikeDto rLikeDto);
+
+}
