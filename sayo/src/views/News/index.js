59903100189f85a38ea2c910ccad2ee3aae1f9b5
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-11-08 22:49:57 +0900

    Create index.js

---
+++ sayo/src/views/News/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WorldPlaces from "../WorldPlaces";
+
+export default function News() {
+
+    return (
+        <div style={{marginTop:'20px'}}>
+            <WorldPlaces/>
+        </div>
+        );
+}
