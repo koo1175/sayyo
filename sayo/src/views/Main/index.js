16318d035703e9ea9c42cdfc7db141c0c0594009
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-11-08 22:49:24 +0900

    Update index.js

--- sayo/src/views/Main/index.js
+++ sayo/src/views/Main/index.js
@@ -1,16 +1,15 @@
 import React from "react";
+import PopUPModal from "../PopUpModal";
+import Map from "../Map"
 
 export default function Main() {
 
     return (
-        <div style={{width: '100%',
-            height: '100%',
-            backgroundColor:'blue',
-            backgroundSize: 'cover',
-            color: 'white',
-            fontSize: '70px',
-            marginTop:'20px'}}>
-            MainPage
+        <div>
+        <div style={{ marginTop:'20px' }}>
+            <PopUPModal/>
+        </div>
+        <Map/>
         </div>
         );
 }
