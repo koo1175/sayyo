eeac464feda464bc5f411a01bd6e9c1402623149
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-11-08 22:49:41 +0900

    Update index.js

--- sayo/src/views/MockElectionComponent/index.js
+++ sayo/src/views/MockElectionComponent/index.js
@@ -1,9 +1,36 @@
 import React from "react";
-export default function MockElectionComponent() {
+// import WorldPlaces from "../WorldPlaces";
+import SiblingFade from "../SiblingFade";
 
+export default function MockElectionComponent() {
     return (
-        <div style={{ marginTop:'20px' }}>
-            모의투표 페이지 입니다.
+        <div style={{ backgroundColor: 'white' }}>
+            <div style={{ marginTop: '3%' }}>
+                <img
+                    alt="titlemok"
+                    src="/img/titlemok.png"
+                    style={{ width: '50%' }}
+                />
+            </div>
+                <div style={{                }}>
+                    {/* <div style={centerImageStyle}>
+        <button
+          onClick={goSecretPage} // 버튼 클릭 시 이 이벤트를 처리
+          style={{
+            border: 'none',
+            background: 'none',
+            cursor: 'pointer'
+          }}
+        > */}
+                    <img
+                        alt="btnMok"
+                        src="/img/btnMok.png"
+                        style={{ width: '200px' }}
+                    />
+                    {/* </button> */}
+                    {/* </div> */}
+                </div>
+                <SiblingFade />
         </div>
-        );
+    );
 }
