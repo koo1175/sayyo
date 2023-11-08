dcaae225dfc05b53e8c3b1e143e1a2dab4240158
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-11-08 22:50:07 +0900

    Create index.js

---
+++ sayo/src/views/Info/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WorldPlaces from "../WorldPlaces";
+
+export default function Info() {
+
+    return (
+        <div style={{marginTop:'20px'}}>
+            <WorldPlaces/>
+        </div>
+        );
+}
