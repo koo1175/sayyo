4cbff2082aa4a7d878511214faf0083414b062d9
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-11-08 22:50:31 +0900

    Create index.js

---
+++ sayo/src/views/Promise/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WorldPlaces from "../WorldPlaces";
+
+export default function Promise() {
+
+    return (
+        <div style={{marginTop:'20px'}}>
+            <WorldPlaces/>
+        </div>
+        );
+}
