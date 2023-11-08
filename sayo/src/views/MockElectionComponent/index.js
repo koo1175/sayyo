f2bdc517fdb8feacf9f2b70e52b69b97a9f94798
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-11-08 10:28:19 +0900

    Create index.js

---
+++ sayo/src/views/MockElectionComponent/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function MockElectionComponent() {
+
+    return (
+        <div style={{ marginTop:'20px' }}>
+            모의투표 페이지 입니다.
+        </div>
+        );
+}
