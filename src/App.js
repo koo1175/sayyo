919fb6230a7b244f93f09a2ad345e62a483e0c90
Huiyeon <gmldus0814@naver.com>
2023-11-30 11:53:48 +0900

    머지통합

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD:sayo/src/App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './views/Navigation';
 import Copyright from './views/Copyright';
 import KaKaoHandeler from './views/AuthForm';
 import PopUPModal from './views/PopUpModal';
-=======
-import Chat from "./views/Chat";
->>>>>>> origin/suchan:src/App.js
 
 function App() {
 
   return (
     <div className="App" style={{ position: 'relative', minHeight: '100vh' }}>
       <Copyright/>
-<<<<<<< HEAD:sayo/src/App.js
       <Navigation />
       <Router>
         <Routes>
@@ -22,9 +17,6 @@ function App() {
           <Route exact path="/" component={<PopUPModal/>} />
         </Routes>
       </Router>
-=======
-      
->>>>>>> origin/suchan:src/App.js
     </div>
   );
 }
