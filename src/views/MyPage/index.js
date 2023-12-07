d86f3111aa34c52b876dc2b8a700117bf55ea9cb
Huiyeon <gmldus0814@naver.com>
2023-12-08 01:21:27 +0900

    2023.12.08

--- src/views/MyPage/index.js
+++ src/views/MyPage/index.js
@@ -14,16 +14,16 @@ export default function MyPage () {
   const multiLineText = "'세요' 사이트에서는 개인정보보호정책에 따라 중요정보 접근시 회원의 비밀번호 인증 절차를 수행해야 합니다.\n\n회원정보 수정을 진행하시려면 로그인 시 입력하신 비밀번호로 인증 절차를 수행해 주시기 바랍니다.";
 
   return (
-    <div style={{width:1400, marginTop:'120px', marginLeft:'15%'}}>
+    <div style={{width:1400, marginTop:'170px', marginLeft:'15%'}}>
     <div className="member-info-container">
       <div className="page-title">
-        <div style={{ display: 'flex', justifyContent: 'flex-start', marginLeft: -150}}>
+        <div style={{ display: 'flex', justifyContent: 'flex-start', marginLeft: -150, fontFamily:'BookkGothic-Bd'}}>
         <h2>회원정보관리</h2>
         </div>
         <hr style={{width:950, marginLeft:-150}} />
       </div>
       <div className="section">
-      <div style={{ display: 'flex', justifyContent: 'flex-start', marginLeft: -150, marginTop:-70}}>
+      <div style={{ display: 'flex', justifyContent: 'flex-start', marginLeft: -150, marginTop:-60, fontFamily:'BookkGothic-Bd'}}>
         <h3>회원 정보 수정 - 회원 인증</h3>
         </div>
           <div className="password-box" style={{textAlign:'center', color:'white', fontWeight:'bold', fontSize:'18px', paddingTop:10, height: 40, width: 950, marginLeft:-150}}>비밀번호</div>
@@ -34,13 +34,13 @@ export default function MyPage () {
       <div className="info-box">
         <div className="policy-box" style={{ width:900, marginLeft:-150, border: '0.5px solid #d9d9d9'}}>
           <div style={{display: 'flex', justifyContent: 'flex-start', color:'#0E3B64', fontWeight:'bold', fontSize:18}}>개인정보보호정책에 따른 인증절차 강화 안내</div>
-          <div style={{display: 'flex', justifyContent: 'flex-start', backgroundColor:'white', width:900, height:80, marginLeft:-20, marginTop:15, padding:20, borderTop: '0.5px solid #d9d9d9'}}>
+          <div style={{display: 'flex', justifyContent: 'flex-start', backgroundColor:'white', width:900, height:110, marginLeft:-20, marginTop:15, padding:20, borderTop: '0.5px solid #d9d9d9'}}>
           <div style={{whiteSpace: 'pre-wrap', textAlign:'left',}}>{multiLineText}</div>
           </div>
         </div>
         <br/>
-        <div className="password-input" style={{ width:900, height:100, marginLeft:-150, border: '0.5px solid #d9d9d9', padding:20}}>        
-         <div style={{marginTop:30, fontWeight:'bold'}}> 비밀번호 입력 <input type='password' style={{marginLeft:10 ,width:300, height:30, fontSize:20}}>
+        <div className="password-input" style={{ width:900, height:80, marginLeft:-150, border: '0.5px solid #d9d9d9', padding:20}}>        
+         <div style={{marginTop:5, fontWeight:'bold'}}> 비밀번호 입력 <input type='password' style={{marginLeft:10 ,width:300, height:30, fontSize:20}}>
            </input></div>
         </div>
         <div className="button-box">
